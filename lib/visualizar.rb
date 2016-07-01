9deca592d252855eb412a3962a3ef793d649e032
hugoe29 <hugoe29@hotmail.com>
2016-07-01 11:27:50 -0500

    chidoscambios en visualizar

---
+++ lib/visualizar.rb
@@ -0,0 +1,6 @@
+class Visualizar
+  def visualiza rows = 2,columns = 2
+    tablero = [[[0,0],[0,0]],
+              [[0,0],[0,0]]]
+  end
+end
--- spec/visualizar_spec.rb
+++ spec/visualizar_spec.rb
@@ -1,10 +1,12 @@
+require './lib/visualizar'
+
 describe Visualizar do
   it "Visualizar tablero de 6x6" do
     tablero = [[[0,0],[0,0]],
               [[0,0],[0,0]]]
     visualizar = Visualizar.new
     resultado = visualizar.visualiza 6,6
-    resultado.should = tablero
+    resultado.should == tablero
   end
 
 end
