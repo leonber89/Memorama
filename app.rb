4247114308be81fc8c9019085e47e232694b81c2
Equipo 1 <leonber89.1@gmail.com>
2016-07-01 10:13:13 -0500

    Se esta agregando un nuevo feature, un view de inicio y los steps del feature

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
 
 get '/' do
-    
+    erb :found
 end
---
+++ features/step_definitions/my_steps.rb
@@ -0,0 +1,15 @@
+Given(/^que abri la aplicacion$/) do
+  visit '/'
+end
+
+Then(/^debo ver "(.*?)"$/) do |text|
+  last_response.body.should =~ /#{text}/m
+end
+
+When(/^selecciono "(.*?)"$/) do |name|
+  click_button(name)
+end
+
+Then(/^crea tablero$/) do
+  
+end
