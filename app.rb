ef9c2b8dca20d896ac4745d8f781d87da8ea7d3e
hugoe29 <hugoe29@hotmail.com>
2016-07-01 12:53:17 -0500

    visualizar mas cambios

--- app.rb
+++ app.rb
@@ -8,8 +8,13 @@ get '/' do
 end
 
 post '/inicia' do
-  session["tablero"] = Tablero.new params["rows"].to_i, params["columns"].to_i,0
-  session["rows"] = session["tablero"].rows
-  session["columns"] = session["tablero"].columns
-  erb :juego
+  resultado = (params["rows"].to_i * params["columns"].to_i)%2
+  if resultado == 0
+    session["tablero"] = Tablero.new params["rows"].to_i, params["columns"].to_i,0
+    session["rows"] = session["tablero"].rows
+    session["columns"] = session["tablero"].columns
+    erb :juego
+  else
+    erb :found
+  end
 end
